f2dc9ca80c5b433f85ad754cfb94f9ff0b955c43
aasif-create <aasifmmd12345@gmail.com>
2025-08-23 23:48:11 +0530

    UPDATED THE CODE FROM VS TO GITHUB

---
+++ BASIC_PATTERNS.java
@@ -0,0 +1,56 @@
+
+
+public class BASIC_PATTERNS
+{
+    public static void main(String[] args)
+    {
+        System.out.println("1. SOLID RECTANGLE(*)");
+        for(int i=1;i<5;i++){
+            for(int j =0;j<5;j++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+        System.out.println("<--------------------------------------------------------------->");
+
+        System.out.println("2. HOLLOW RECTANGLE(*)");
+        int n=5;
+        int m=6;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++) {
+                if (i == 1 || j == 1 || i == n || j == m)
+                    System.out.print(" *");
+                else
+                    System.out.print("  ");
+            }
+                System.out.println();
+        }
+
+        System.out.println("<--------------------------------------------------------------->");
+
+        System.out.println("3.LEFT HALF PYRAMID (*)");
+        int a = 6;
+        for(int i=1 ;i<a;i++)
+        {
+            for(int j=1 ; j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+        System.out.println("<--------------------------------------------------------------->");
+        System.out.println("4.INVERTED HALF PYRAMID (*)");
+        for(int i=n ;i>1;i--) {
+            for (int j = 1; j <i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+        }
+         System.out.println("<--------------------------------------------------------------->");
+
+    }
+}
